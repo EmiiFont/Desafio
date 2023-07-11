c68f18c1aae0d07771951491e26dba727d8121d0
Emilio <emifont01@gmail.com>
2023-07-10 21:08:22 -0400

    chore: clean ups

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"desafio/server"
-	"io"
 	"log"
 	"net/http"
 
@@ -16,22 +15,6 @@ func main() {
 	}
 	//configure env variables
 	server.SetupServer()
-
-	// Hello world, the web server
-	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Hello, world!\n")
-	}
-
-	// getQuestionHandler := func(w http.ResponseWriter, req *http.Request) {
-	// 	opt := []int{1, 2}
-	// 	questionNas := Question{Answer: "Emilio", Id: "1", Ask: "What's your name", Options: opt}
-	// 	marshalled, _ := json.Marshal(questionNas)
-	// 	fmt.Println(string(marshalled))
-	// 	io.WriteString(w, string(marshalled))
-	// }
-	http.HandleFunc("/hello", helloHandler)
-	//http.HandleFunc("/getQuestion", getQuestionHandler)
-	//log.Println("Listing for requests at http://localhost:8000/hello")
 	log.Println("Starting game server... on port 8000")
 	err = http.ListenAndServe(":8000", nil)
 	if err != nil {
